4a5645d224611dbab61b623ac7e96bdb73b0159f
aarpathacoder <98888655+aarpathacoder@users.noreply.github.com>
2022-02-02 18:22:01 +0530

    Add files via upload

---
+++ Interviewque.js
@@ -0,0 +1,21 @@
+// Interview Que 1.
+
+// 1. Difference between null vs undefined?
+// Null is a value assigned to a variable which means nothing.
+// undefined is something like if u create a variable n dont assign any value to it then it is undefined.Difference
+
+
+
+// 2. What is NaN?
+// NaN is a property of the global Object.
+// In other words, it is a variable in global Scope.
+// The initial value of NaN is Not-A-Number.
+
+
+
+// 3. What is the differene between ==  vs === ?
+// Here in == it nly checks the value .
+// Whereas in === it also check the type of datatype and also value.
+
+
+
